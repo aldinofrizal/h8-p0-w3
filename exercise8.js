dc082edce8dfd164dca896643aab7dc7a7780590
aldinofrizal <aldinofrizal@gmail.com>
2019-07-30 15:20:38 +0700

    exercise8

---
+++ exercise8.js
@@ -0,0 +1,26 @@
+
+function pasanganTerbesar(num) {
+    // you can only write your code here!
+    var value = []; //empty array to hold each number data
+    numStr = String(num); //string of number data
+    
+    for (var i = 0; i < numStr.length-1; i++){
+        value.push(Number(numStr.substr(i,2))); //push each number to array
+    }
+    var maxValue = 0; //comparator to each array element
+    for (var i = 0; i < value.length; i++){
+        if (maxValue <= value[i]){
+            maxValue = value[i];
+        }else{
+            maxValue = maxValue;
+        }
+    }
+    return maxValue;        
+} 
+  
+  // TEST CASES
+  console.log(pasanganTerbesar(641573)); // 73
+  console.log(pasanganTerbesar(12783456)); // 83
+  console.log(pasanganTerbesar(910233)); // 91
+  console.log(pasanganTerbesar(71856421)); // 85
+  console.log(pasanganTerbesar(79918293)); // 99
