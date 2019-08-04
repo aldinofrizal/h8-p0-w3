0ad14f30cb1f72d504ed977ec3a1214b78ce9915
aldinofrizal <aldinofrizal@gmail.com>
2019-08-04 17:35:40 +0700

    revisi exercise2

--- exercise2.js
+++ exercise2.js
@@ -1,11 +1,13 @@
 //input "hello world!"
 //output
 //"!dlrow olleh"
-
-var str = 'hello world!';
-var strArr = [];
-for (var i = 0; i < str.length; i++){
-    strArr.push(str[str.length-i-1]);
+function balikString (str){
+    var strArr = [];
+    for (var i = 0; i < str.length; i++){
+        strArr.push(str[str.length-i-1]);
+    }
+    var output = strArr.join('');
+    return output;
 }
-var output = strArr.join('');
-console.log(output);
+
+console.log(balikString("hello world!"));
